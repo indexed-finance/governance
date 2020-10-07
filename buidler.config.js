0659c746e98d367fef4daa188d9d877dbdc16a31
dillon <dillonkellar@protonmail.com>
2020-10-07 01:47:55 -0500

    Updated buidler config

---
+++ buidler.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const url = require('url');
+
+require('dotenv').config();
+
+const { InfuraProvider } = require('@ethersproject/providers');
+const { fromPrivateKey } = require('ethereumjs-wallet');
+const { randomBytes } = require('crypto');
+
+usePlugin("@nomiclabs/buidler-waffle");
+usePlugin("buidler-ethers-v5");
+usePlugin("buidler-deploy");
+usePlugin('buidler-abi-exporter');
+usePlugin("solidity-coverage");
+
+const keys = {
+  rinkeby: fromPrivateKey(
+    process.env.RINKEBY_PVT_KEY
+      ? Buffer.from(process.env.RINKEBY_PVT_KEY.slice(2), 'hex')
+      : randomBytes(32)).getPrivateKeyString()
+};
+
+module.exports = {
+  namedAccounts: {
+    deployer: {
+      default: 0
+    },
+  },
+  networks: {
+    buidlerevm: {
+      live: false,
+      saveDeployment: false
+    },
+    local: {
+      url: url.format({
+        protocol: "http:",
+        port: 8545,
+        hostname: "localhost",
+      }),
+    },
+    rinkeby: {
+      url: new InfuraProvider("rinkeby", process.env.INFURA_PROJECT_ID).connection.url,
+      accounts: [keys.rinkeby],
+      chainId: 4
+    },
+    coverage: {
+      url: url.format({
+        protocol: "http:",
+        port: 8555,
+        hostname: "localhost",
+      }),
+    }
+  },
+  paths: {
+    sources: path.join(__dirname, 'contracts'),
+    tests: path.join(__dirname, 'test'),
+    cache: path.join(__dirname, 'cache'),
+    artifacts: path.join(__dirname, 'artifacts'),
+    deploy: path.join(__dirname, "deploy"),
+    deployments: path.join(__dirname, "deployments")
+  },
+  solc: {
+    version: "0.6.8",
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+};
