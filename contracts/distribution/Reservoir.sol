0fd33ef08e957b3c4309795662cb04e9fecc8260
dillon <dillonkellar@protonmail.com>
2020-10-12 12:58:37 -0500

    Added reservoir contract for block-based reward distribution.

---
+++ contracts/distribution/Reservoir.sol
@@ -0,0 +1,139 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+/*
+Copyright 2020 Compound Labs, Inc.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
+following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or
+promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+/**
+ * @title Reservoir Contract
+ * @dev Distributes a token to a different contract at a fixed rate.
+ * @dev This contract must be poked via the `drip()` function every so often.
+ * @author Compound
+ */
+contract Reservoir {
+  using SafeERC20 for IERC20;
+
+  /**
+   * @dev The block number when the Reservoir started (immutable)
+   */
+  uint public immutable dripStart;
+
+  /**
+   * @dev Tokens per block that to drip to target (immutable)
+   */
+  uint public immutable dripRate;
+
+  /**
+   * @dev Reference to token to drip (immutable)
+   */
+  IERC20 public immutable token;
+
+  /**
+   * @dev Target to receive dripped tokens (immutable)
+   */
+  address public immutable target;
+
+  /**
+   * @dev Amount that has already been dripped
+   */
+  uint public dripped;
+
+  /**
+    * @dev Constructs a Reservoir
+    * @param dripRate_ Numer of tokens per block to drip
+    * @param token_ The token to drip
+    * @param target_ The recipient of dripped tokens
+    */
+  constructor(uint dripRate_, IERC20 token_, address target_) public {
+    dripStart = block.number;
+    dripRate = dripRate_;
+    token = token_;
+    target = target_;
+    dripped = 0;
+  }
+
+  /**
+    * @dev Drips the maximum amount of tokens to match the drip rate since inception
+    * @dev Note: this will only drip up to the amount of tokens available.
+    * @return The amount of tokens dripped in this call
+    */
+  function drip() public returns (uint) {
+    // First, read storage into memory
+    IERC20 token_ = token;
+    uint reservoirBalance_ = token_.balanceOf(address(this));
+    uint dripRate_ = dripRate;
+    uint dripStart_ = dripStart;
+    uint dripped_ = dripped;
+    address target_ = target;
+    uint blockNumber_ = block.number;
+
+    // Next, calculate intermediate values
+    uint dripTotal_ = mul(dripRate_, blockNumber_ - dripStart_, "dripTotal overflow");
+    uint deltaDrip_ = sub(dripTotal_, dripped_, "deltaDrip underflow");
+    uint toDrip_ = min(reservoirBalance_, deltaDrip_);
+    uint drippedNext_ = add(dripped_, toDrip_, "tautological");
+
+    // Finally, write new `dripped` value and transfer tokens to target
+    dripped = drippedNext_;
+    token_.safeTransfer(target_, toDrip_);
+
+    return toDrip_;
+  }
+
+  /* Internal helper functions for safe math */
+
+  function add(uint a, uint b, string memory errorMessage) internal pure returns (uint) {
+    uint c = a + b;
+    require(c >= a, errorMessage);
+    return c;
+  }
+
+  function sub(uint a, uint b, string memory errorMessage) internal pure returns (uint) {
+    require(b <= a, errorMessage);
+    uint c = a - b;
+    return c;
+  }
+
+  function mul(uint a, uint b, string memory errorMessage) internal pure returns (uint) {
+    if (a == 0) {
+      return 0;
+    }
+    uint c = a * b;
+    require(c / a == b, errorMessage);
+    return c;
+  }
+
+  function min(uint a, uint b) internal pure returns (uint) {
+    if (a <= b) {
+      return a;
+    } else {
+      return b;
+    }
+  }
+}
