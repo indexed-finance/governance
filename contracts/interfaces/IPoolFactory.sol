637b97c2339e85f24f71777f5f01240f8517dbc6
dillon <dillonkellar@protonmail.com>
2020-11-15 09:41:13 -0600

    Updated pool factory interface

--- contracts/interfaces/IPoolFactory.sol
+++ contracts/interfaces/IPoolFactory.sol
@@ -2,5 +2,5 @@ pragma solidity ^0.6.0;
 
 
 interface IPoolFactory {
-  function isIPool(address pool) external view returns (bool);
+  function isRecognizedPool(address pool) external view returns (bool);
 }
